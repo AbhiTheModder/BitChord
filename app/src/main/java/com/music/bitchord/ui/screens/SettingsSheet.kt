88d4e422c977fdce54d9abda2f8e67aa5c38882b
Shubhang krishan <krishanshubhang@gmail.com>
2026-09-14 21:39:27 +0530

    feat: add website link to settings

--- app/src/main/java/com/music/bitchord/ui/screens/SettingsSheet.kt
+++ app/src/main/java/com/music/bitchord/ui/screens/SettingsSheet.kt
@@ -1079,6 +1079,10 @@ fun SettingsScreen(
                 withLink(LinkAnnotation.Url("https://discord.gg/pDdKfrdHY6", linkStyles)) {
                     append("Discord")
                 }
+                append("  ")
+                withLink(LinkAnnotation.Url("https://bitchord.kushagrasingh.in/", linkStyles)) {
+                    append("Website")
+                }
                 append("\n~YouTube Music Backend")
             },
             style = MaterialTheme.typography.labelSmall,
